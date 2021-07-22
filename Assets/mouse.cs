50a8d6731c8f2503549a308753ac09abf7afc25f
AmreetKumarkhuntia <79908976+AmreetKumarkhuntia@users.noreply.github.com>
2021-07-22 13:10:22 +0530

    Character commit intial
    Few 3D blocks with the cs script

---
+++ Assets/mouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse : MonoBehaviour
+{
+    public float mousesensitivity=1f;
+    public Transform playerbody;
+    float xRotation = 0f;
+    // Update is called once per frame
+    void start()
+    {
+        Cursor.lockState= CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        
+        float mousex = Input.GetAxis("Mouse X")*mousesensitivity*Time.deltaTime;
+        float mousey = Input.GetAxis("Mouse Y")*mousesensitivity*Time.deltaTime;
+        
+        xRotation-=mousey;
+        xRotation=Mathf.Clamp(xRotation,-90f,90f);
+
+        transform.localRotation  = Quaternion.Euler(xRotation, 0f,0f);
+        playerbody.Rotate(Vector3.up*mousex);
+    }
+}
